f8835a87f04d9dabbedaed4cdb040f05cc3a2e75
Nathan Sculli <scull7@gmail.com>
2015-06-24 18:03:33 -0700

    export lock error object so that dependent modules can use it in exception blocks.

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 var Store           = require('./lib/store.js');
 var MemoryDriver    = require('./lib/driver-memory.js');
+var LockError       = require('uow-lock').LockError;
 
 Store.MemoryDriver  = MemoryDriver;
+Store.LockError     = LockError;
 
 /**
  * Memory based storage example of using the Store object.
